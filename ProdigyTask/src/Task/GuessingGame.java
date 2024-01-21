ed1d4f05b293cc94291bf90f9dbebf9a25f753bb
Mohamad Kaif <kaifmohamad169@gmai.com>
2024-01-21 16:15:36 +0530

    First Commit

---
+++ ProdigyTask/src/Task/GuessingGame.java
@@ -0,0 +1,38 @@
+package Task;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int minRange = 1;
+        int maxRange = 100;
+        int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+        int userGuess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("Try to guess the number between " + minRange + " and " + maxRange);
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number.");
+                System.out.println("It took you " + attempts + " attempts to win the game.");
+            }
+
+        } while (userGuess != randomNumber);
+
+        scanner.close();
+    }
+}
